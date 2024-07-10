e6cdd75380d2ad9f8a61fc86093feb165d16b1e9
Vrunda Desai <Vrundadesai1308@gmail.com>
2024-07-09 18:34:42 -0600

    Initial commit

---
+++ Utilities/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalProjectC.Utilities
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new StudentContext())
+            {
+                var student = new student
+                {
+                    FirstName = "John",
+                    LastName = "Doe",
+                    EnrollmentDate = DateTime.Now
+                };
+
+                context.Students.Add(student);
+                context.SaveChanges();
+
+                Console.WriteLine("Student saved to the database.");
+                var students = context.Students.ToList();
+                foreach (var s in students)
+                {
+                    Console.WriteLine($"{s.StudentId}: {s.FirstName} {s.LastName}, Enrolled: {s.EnrollmentDate}");
+                }
+            }
+        }
+    }
+}
---
+++ Utilities/student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalProjectC.Utilities
+{
+    public class student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
